54a0b7242857d74871fd33e2303b0793276b7afd
leoleoleo1123 <leoelcrack38@gmail.com>
2024-02-27 17:34:59 -0600

    first commit

---
+++ src/components/CounterFile.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="text-center">
+    <h1 style="margin-block-end: 5px;">{{ counter }}</h1>
+    <button @click="incremento" style="margin-bottom:10px;"> sumar </button>
+    <button @click="reset" style="margin-top:10px;"> reset </button>
+
+    </div>
+    <div>
+    <img :src="require('../assets/img/random.jpg')" alt="random" style="width: 500px; height: auto;">
+    </div>
+</template>
+
+<script>
+
+    import { ref } from'vue';
+
+    export default{
+    setup() {
+        const counter= ref(0);
+        return{counter};
+    },
+
+    methods: {
+        incremento(){
+            this.counter++;
+        },
+
+        reset(){
+            this.counter = 0;
+        }
+    }
+    }
+</script>
+
+<style>
+
+.content-center {
+    display: flex;
+    flex-direction: column;
+    align-items: center; 
+    justify-content: center;
+    height: 220px;
+    text-align: center;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    Hello World
+   <h3> {{ info }} </h3> 
+  </div>
+    
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      info: 'Leonardo Javier Molina Diaz 20988 5A'
+    }
+  }
+}
+</script>
+
+<style>
+.content-center {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+    text-align: center; 
+   
+}
+</style>
